de3f38f19e5a5b19cdc2820eeac3a2c4e7ad9795
soul98 <soulaymaane@gmail.com>
2026-03-23 05:13:51 +0100

    remove number

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
             CarRental
           </Link>
 
-          <a
+          {/* <a
             href='tel:+1234567890'
             className='hidden lg:flex items-center gap-1.5 text-gray-400 text-xs hover:text-blue-600 transition-colors'
           >
@@ -67,8 +67,8 @@ const Navbar = () => {
                 d='M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z'
               />
             </svg>
-            +1-234-567-890
-          </a>
+          
+          </a> */}
         </div>
 
         {/* Center — Desktop Nav */}
